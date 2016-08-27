f323da803f9e1c337ad4719bcae1f9e04028c6f3
theSoenke <soenke.behrendt@outlook.com>
2016-08-27 16:42:11 +0200

    Start level importer

---
+++ Assets/Scripts/LevelImporter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LevelImporter
+{
+    public static Rail[,] GetRailGridFromFile(TextAsset level)
+    {
+        var grid = new Rail[1, 1];
+        return grid;
+    }
+}
