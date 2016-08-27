9c447c99d806ad519e47d47f408d76374f0d4b20
kautschuckflasche <nilsw.agner@web.de>
2016-08-27 15:27:30 +0200

    i don't like git

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -3,19 +3,16 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-    public event EventHandler OnLoadComplete;
 
     private TrainController _trainController;
+    private RailGrid _railGrid;
 	
 	void Start ()
     {
         #region init
         _trainController = new TrainController();
         #endregion
-
-
-        if (OnLoadComplete != null)
-            OnLoadComplete(this, null);
+        
     }
 	
 	// Update is called once per frame
