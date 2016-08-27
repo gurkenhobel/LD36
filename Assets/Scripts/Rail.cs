b558587e1644f6574377cd23abebe1e60ecb0401
theSoenke <soenke.behrendt@outlook.com>
2016-08-27 21:51:31 +0200

    Separate rails from rails controller

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Rail
+{
+    protected Vector3 position;
+    protected RailDirection direction;
+    private readonly GameObject cellObject;
+
+    protected enum RailDirection
+    {
+        Left,
+        Right,
+        Top,
+        Bottow
+    }
+
+    public Rail(Transform parent, GameObject prefab, Vector2 pos)
+    {
+        position = new Vector3(pos.x, 0, pos.y);
+        cellObject = Object.Instantiate(prefab, position, Quaternion.identity) as GameObject;
+        cellObject.transform.SetParent(parent);
+    }
+
+    public Vector2 GetPosition()
+    {
+        return position;
+    }
+
+    public List<Vector3> GetWaypoints()
+    {
+        var waypointTransforms = cellObject.GetComponents<Transform>();
+        return waypointTransforms.Select(waypoint => waypoint.position).ToList();
+    }
+}
+
+public class SwitchRail : Rail
+{
+    public SwitchRail(Transform parent, GameObject prefab, Vector2 pos) : base(parent, prefab, pos)
+    {
+
+    }
+
+    public void RotateSwitch()
+    {
+        direction += 1 % 4;
+    }
+}
--- Assets/Scripts/RailGridController.cs
+++ Assets/Scripts/RailGridController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 public class RailGridController : MonoBehaviour
 {
@@ -73,51 +70,5 @@ public class RailGridController : MonoBehaviour
             default:
                 throw new InvalidEnumArgumentException();
         }
-    }
-}
-
-public enum RailDirection
-{
-    Left,
-    Right,
-    Top,
-    Bottow
-}
-
-public class Rail
-{
-    protected Vector3 position;
-    protected RailDirection direction;
-    private readonly GameObject cellObject;
-
-    public Rail(Transform parent, GameObject prefab, Vector2 pos)
-    {
-        position = new Vector3(pos.x, 0, pos.y);
-        cellObject = Object.Instantiate(prefab, position, Quaternion.identity) as GameObject;
-        cellObject.transform.SetParent(parent);
-    }
-
-    public Vector2 GetPosition()
-    {
-        return position;
-    }
-
-    public List<Vector3> GetWaypoints()
-    {
-        var waypointTransforms = cellObject.GetComponents<Transform>();
-        return waypointTransforms.Select(waypoint => waypoint.position).ToList();
-    }
-}
-
-public class SwitchRail : Rail
-{
-    public SwitchRail(Transform parent, GameObject prefab, Vector2 pos) : base(parent, prefab, pos)
-    {
-
-    }
-
-    public void RotateSwitch()
-    {
-        direction += 1 % 4;
     }
 }
