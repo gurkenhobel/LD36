8fb74baafa5d738872e41846571264bfcda35672
kautschuckflasche <nilsw.agner@web.de>
2016-08-27 16:47:19 +0200

    :train:

--- Assets/scripts/Train.cs
+++ Assets/scripts/Train.cs
@@ -4,21 +4,34 @@ using System.Collections.Generic;
 
 public class Train : MonoBehaviour
 {
-    private List<GameObject> Trailer;
+    #region prefabs
+    public Trailer TrailerPrefab;
+    #endregion
+    public Trailer Lock;
+
+
+    private List<Trailer> Trailer;
+
+    private const float TRAILER_LENGTH = 3;
 
 	// Use this for initialization
 	void Start ()
     {
-        Trailer = new List<GameObject>();
+        Trailer = new List<Trailer>();
 	}
 	
     public void GenerateTrailers(int count)
     {
-
+        for(int i = 0; i < count; i++)
+        {
+            var trailer = Instantiate<Trailer>(TrailerPrefab);
+            Trailer.Add(trailer);
+        }
     }
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
